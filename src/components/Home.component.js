f44d64d071c4ff82511e86684f238a61ee30c465
Shikher Aatrey <shikher.aatrey@juspay.in>
2017-05-13 01:19:15 +0530

    [modified] auth0 propagating profile info. ready

--- src/components/Home.component.js
+++ src/components/Home.component.js
@@ -4,6 +4,17 @@ import React, { Component } from 'react';
 import AuthService from '../utils/AuthService.util';
 
 class Home extends Component {
+	constructor(props, context) {
+    	super(props, context)
+    	this.state = {
+      		profile: props.auth.getProfile()
+    	}
+
+    	props.auth.on('profile_incoming', (profile) => {
+    		console.log(profile);
+    	});
+ 	}
+
 	render() {
 		return (
 			<div>
--- src/utils/AuthService.util.js
+++ src/utils/AuthService.util.js
@@ -1,51 +1,81 @@
-import Auth0Lock from 'auth0-lock'
-import { browserHistory } from 'react-router'
+// AuthService Helper Class
 
-export default class AuthService {
-  constructor(clientId, domain) {
-    // Configure Auth0
-    this.lock = new Auth0Lock(clientId, domain, {
-      auth: {
-        redirectUrl: 'http://localhost:5000/login',
-        responseType: 'token'
-      }
-    })
-    // Add callback for lock `authenticated` event
-    this.lock.on('authenticated', this._doAuthentication.bind(this))
-    // binds login functions to keep this context
-    this.login = this.login.bind(this)
-  }
+// bringing in the dependancies
+import { EventEmitter } from 'events';
+import Auth0Lock from 'auth0-lock';
+import { browserHistory } from 'react-router';
 
-  _doAuthentication(authResult) {
-    // Saves the user token
-    this.setToken(authResult.idToken)
-    // navigate to the home route
-    browserHistory.replace('/home')
-  }
+class AuthService extends EventEmitter {
+	constructor(clientId, domain) {
+		super();
+	    // configure Auth0
+	    this.lock = new Auth0Lock(clientId, domain, {
+	    	auth: {
+	    		redirectUrl: 'http://localhost:5000/login',
+	    		responseType: 'token'
+	    	}
+	    });
+	    // add callback for lock `authenticated` event
+	    this.lock.on('authenticated', this._doAuthentication.bind(this));
+	    // binds login functions to keep this context
+	    this.login = this.login.bind(this);
+	}
 
-  login() {
-    // Call the show method to display the widget.
-    this.lock.show()
-  }
+	_doAuthentication(authResult) {
+	    // saves the user token
+	    this.setToken(authResult.idToken);
+	    // navigate to the home route
+	    browserHistory.replace('/home');
+	    // loads the user profile data
+	    this.lock.getProfile(authResult.idToken, (error, profile) => {
+	    	if (error) {
+	    		console.log('Error loading the profile', error);
+	    	} else {
+	    		console.log(profile);
+	    		this.setProfile(profile);
+	    	}
+	    })
+	}
 
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    return !!this.getToken()
-  }
+	setProfile(profile) {
+	  	// saves profile data to local storage
+	  	localStorage.setItem('profile', JSON.stringify(profile));
+	  	// triggers event to update UI and look from DB
+	  	this.emit('profile_incoming', profile);
+	}
 
-  setToken(idToken) {
-    // Saves user token to local storage
-    localStorage.setItem('id_token', idToken)
-  }
+	getProfile() {
+	  	// retrieves profile data from local storage
+	  	const profile = localStorage.getItem('profile');
+	  	return profile ? JSON.parse(localStorage.profile) : {};
+	}
 
-  getToken() {
-    // Retrieves the user token from local storage
-    return localStorage.getItem('id_token')
-  }
+	login() {
+	    // call the show method to display the widget.
+	    this.lock.show();
+	}
 
-  logout() {
-    // Clear user token and profile data from local storage
-    localStorage.removeItem('id_token');
-    browserHistory.replace('/login');
-  }
+	loggedIn() {
+	    // checks if there is a saved token and it's still valid
+	    return !!this.getToken();
+	}
+
+	setToken(idToken) {
+	    // saves user token to local storage
+	    localStorage.setItem('id_token', idToken);
+	}
+
+	getToken() {
+	    // retrieves the user token from local storage
+	    return localStorage.getItem('id_token');
+	}
+
+	logout() {
+	    // clear user token and profile data from local storage
+	    localStorage.removeItem('id_token');
+	    localStorage.removeItem('profile');
+	    browserHistory.replace('/login');
+	}
 }
+
+export default AuthService;
