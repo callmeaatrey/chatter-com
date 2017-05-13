a98d9dc10ee26ac03bcd7aba2deb02d59d3007e4
Shikher Aatrey <shikher.aatrey@juspay.in>
2017-05-13 20:04:29 +0530

    [modified] grids integrated and ui migrated to antd

--- src/app-client.js
+++ src/app-client.js
@@ -1,14 +1,22 @@
+// Initialiser
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
 import makeRoutes from './routes';
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 
 import App from './containers/App.container';
 
+
+// bring in the routes
 const routes = makeRoutes();
 
 ReactDOM.render(
-	<App history={browserHistory}
-		routes={routes} />,
-	document.getElementById('main')
+	<LocaleProvider locale={enUS}>
+		<App history={browserHistory}
+			routes={routes} />
+	</LocaleProvider>,
+	document.getElementById('main') // this is where our React app lives
 );
