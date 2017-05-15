4edbaa47b67f9a7a4236d5188516b382b42da9d1
Shikher Aatrey <shikher.aatrey@juspay.in>
2017-05-15 13:45:15 +0530

    [modified] onLogin fetching posts fixed

--- src/apis/user.api.js
+++ src/apis/user.api.js
@@ -2,19 +2,26 @@
 
 import axios from 'axios';
 import { newLogin } from '../actions/user.actions';
+import { getOwnPosts } from '../apis/post.api';
 import store from '../store';
 
 // checks if a profile already exists
 export function readProfileViaGoogle(profile) {
 	console.log(profile);
 
+	if(typeof profile.email == undefined) {
+		return;
+	}
 	// handling requests for fetching user profiles or adding new profiles
 	axios.get(`http://localhost:5100/api/user/${profile.email}`)
 		.then(res => {
 			if(res.data == null) {
+
 				createProfileViaGoogle(profile);
 			} else {
+				console.log(profile);
 				store.dispatch(newLogin(profile, res.data));
+    			getOwnPosts(profile.email);
 			}
 		})
 		.catch(err => {
@@ -24,7 +31,7 @@ export function readProfileViaGoogle(profile) {
 
 // creates a new profile
 export function createProfileViaGoogle(profile) {
-
+	console.log(profile);
 	// handling requests for creating new profile
 	axios.post('http://localhost:5100/api/register/google', profile)
 		.then(res => {
